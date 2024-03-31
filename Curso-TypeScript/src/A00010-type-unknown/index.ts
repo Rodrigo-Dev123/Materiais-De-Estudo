4f07d5e6512203dae699578e06e0bb4f87d41760
Rodrigo-Dev123 <rodrigojsg9@gmail.com>
2024-03-31 15:03:37 -0300

    added new changes

---
+++ Curso-TypeScript/src/A00010-type-unknown/index.ts
@@ -0,0 +1,10 @@
+let x: unknown;
+
+x = 100;
+x = 'Rodrigo';
+x = 900;
+x = '10';
+
+const y = 800;
+
+console.log(x + y);
--- Curso-TypeScript/src/A0009-type-enum/index.ts
+++ Curso-TypeScript/src/A0009-type-enum/index.ts
@@ -1,7 +1,19 @@
 enum Cores {
-  VERMELHO, // 0
-  AZUL, // 1
-  AMARELO, // 2
+  VERMELHO = 10, // 10
 }
 
-console.log(Cores);
+enum Cores {
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+  ROXO = 'ROXO',
+}
+
+// console.log(Cores);
+// console.log(Cores[2]);
+// console.log(Cores.ROXO);
+
+function escolhaAcor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaAcor(1234567);
