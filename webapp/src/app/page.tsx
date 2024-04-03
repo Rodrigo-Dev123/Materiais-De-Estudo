5744eeb0f421e4a4aa06deccf5d759b6e40283dd
Rodrigo <rodrigojsg9@gmail.com>
2024-04-03 13:11:12 -0300

    new changes in webapp

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div className='flex h-screen items-center'>
       <h1 className='mx-auto'>Projeto Rodando...</h1>
+      <h1 className='mx-auto'>Olá!!!!!!!!!!</h1>
+    </div>
     </div>
   );
 }
