acac533ab30a2947269ee21b943020249cd13004
Rodrigo <rodrigojsg9@gmail.com>
2024-04-22 18:06:32 -0300

    added new changes from typescript

---
+++ Curso-TypeScript/src/A00022-classes/A00022-classes.ts
@@ -0,0 +1,44 @@
+export class Empresa {
+  public readonly nome: string; // se a palavra public for omitida a propriedade ainda continuará sendo pública por padrão
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    // A classe também pode ser passada como tipo
+    this.colaboradores.push(colaborador);
+  }
+
+  mostraColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Rodrigo', 'Ramos');
+const colaborador2 = new Colaborador('Mariana', 'Souza');
+const colaborador3 = new Colaborador('Ana', 'Oliveira');
+console.log(empresa1);
+// empresa1.nome = 'Facebook'; // A propriedade nome não pode ser atribuida porque é readonly
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+empresa1.adicionaColaborador({
+  nome: 'Rodrigo',
+  sobrenome: 'Ramos',
+});
+console.log(empresa1);
+empresa1.mostraColaboradores();
