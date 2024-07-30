03a4739b6d0e0a06fea6220ce85b731453dcb36d
Rodrigo <rodrigojsg9@gmail.com>
2024-07-30 18:10:00 -0300

    new updates

---
+++ Projeto-Full-Stack-NextJS/app-bd/src/app/components/template/Pagina.tsx
@@ -0,0 +1,7 @@
+export interface PaginaProps {
+    children: any
+}
+
+export default function Pagina(props: PaginaProps) {
+    return <div>{props.children}</div>
+}
---
+++ Projeto-Full-Stack-NextJS/app-bd/src/app/data/constants/usuarios.ts
@@ -0,0 +1,40 @@
+import { Usuario } from "@/core/model/Usuário";
+
+const usuarios: Usuario[] = [
+    {
+        id: '1',
+        email: 'ana@gmail.com',
+        nome: 'Ana',
+        senha: '123456',
+    },
+    {
+        id: '2',
+        email: 'rodrigo@gmail.com',
+        nome: 'Rodrigo',
+        senha: '123456',
+    },
+    {
+        id: '3',
+        email: 'pedro@gmail.com',
+        nome: 'Pedro',
+        senha: '123456',
+    },
+    {
+        id: '4',
+        email: 'Fernanda@gmail.com',
+        nome: 'Fernanda',
+        senha: '123456',
+    },
+    {
+        id: '5',
+        email: 'gabriel@gmail.com',
+        nome: 'Gabriel',
+        senha: '123456',
+    },
+    {
+        id: '6',
+        email: 'rebeca@gmail.com',
+        nome: 'Rebeca',
+        senha: '123456',
+    },
+]
