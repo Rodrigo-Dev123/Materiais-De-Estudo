d24c29dd0aa02c8b1b7be436cbe2d4ba2b05aef1
Rodrigo <rodrigojsg9@gmail.com>
2025-01-22 21:50:55 -0300

    dynamic routes concepts

--- Curso-Python/aula207_ola_django/blog/views.py
+++ Curso-Python/aula207_ola_django/blog/views.py
@@ -16,6 +16,25 @@ def blog(request):
         context
     )
 
+def post(request, post_id):
+    found_post = None
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    print('post')
+
+    context = {
+        'text': 'Estamos no Blog',
+        'posts': [found_post]
+    }
+    return render(
+        request,
+        'blog/index.html',
+        context
+    )
+
 def exemplo(request):
     print('exemplo')
 
