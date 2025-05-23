0cc0cd0fd4ec10bbb6ed321654d88d63e39c53f9
Rodrigo <rodrigojsg9@gmail.com>
2025-05-23 07:23:48 -0300

    new NestJS updates config

--- conceitos_nest/src/app/app.module.ts
+++ conceitos_nest/src/app/app.module.ts
@@ -4,32 +4,25 @@ import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import appConfig from './app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_TYPE: Joi.required(),
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-        DATABASE_USERNAME: Joi.required(),
-        DATABASE_DATABASE: Joi.required(),
-        DATABASE_PASSWORD: Joi.required(),
-        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
-        DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: (appConfigurations: ConfigType<typeof appConfig>) => ({
+        type: appConfigurations.database.type,
+        host: appConfigurations.database.host,
+        port: appConfigurations.database.port,
+        username: appConfigurations.database.username,
+        database: appConfigurations.database.database,
+        password: appConfigurations.database.password,
+        autoLoadEntities: appConfigurations.database.autoLoadEntities,
+        synchronize: appConfigurations.database.synchronize,
       }),
-    }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      database: process.env.DATABASE_DATABASE,
-      password: process.env.DATABASE_PASSWORD,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especifica-las
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o BD. Não deve ser usado em produção
     }),
     RecadosModule,
     PessoasModule,
