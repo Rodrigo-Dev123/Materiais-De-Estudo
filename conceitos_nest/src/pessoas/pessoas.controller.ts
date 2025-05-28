db5c4c01eb7ccf83951cddeb5a4ac2d84e1ec0a6
Rodrigo <rodrigojsg9@gmail.com>
2025-05-28 17:04:31 -0300

    feat: implemented jwt in NestJS

--- conceitos_nest/src/pessoas/pessoas.controller.ts
+++ conceitos_nest/src/pessoas/pessoas.controller.ts
@@ -6,11 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
+import { REQUEST_TOKEN_PAYLOAD_KEY } from 'src/auth/auth.constants';
 
+@UseGuards(AuthTokenGuard)
 @Controller('pessoas')
 export class PessoasController {
   constructor(private readonly pessoasService: PessoasService) {}
@@ -21,7 +26,8 @@ export class PessoasController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Req() req: Request) {
+    console.log(req[REQUEST_TOKEN_PAYLOAD_KEY]);
     return this.pessoasService.findAll();
   }
 
