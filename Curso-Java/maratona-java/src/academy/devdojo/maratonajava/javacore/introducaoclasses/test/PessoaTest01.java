2a579183967cb5d439db7bbfced857c7bed66451
Rodrigo <rodrigo.oliveira@camtecnologia.com.br>
2025-11-27 17:50:53 -0300

    new updates of java course

--- Curso-Java/maratona-java/src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/PessoaTest01.java
+++
@@ -1,9 +0,0 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
-
-import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Pessoa;
-
-public class PessoaTest01 {
-    public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-    }
-}
