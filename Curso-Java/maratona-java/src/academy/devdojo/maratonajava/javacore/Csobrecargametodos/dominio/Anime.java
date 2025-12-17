30e83892c1fe4d8e25a37bd881d8a011d998a454
Rodrigo Ramos <rodrigo.oliveira@camtecnologia.com.br>
2025-12-17 14:42:52 -0300

    new updates of java course

---
+++ Curso-Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -0,0 +1,59 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio;
+
+public class Anime {
+    private String nome;
+    private String tipo;
+    private int espisodios;
+    private String genero;
+
+    public void init(String nome, String tipo, int espisodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.espisodios = espisodios;
+    }
+
+    public void init(String nome, String tipo, int espisodios, String genero) {
+        this.init(nome, tipo, espisodios);
+        this.genero = genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void imprime() {
+        System.out.println("Dragon Ball");
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.espisodios);
+        System.out.println(this.genero);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEspisodios() {
+        return espisodios;
+    }
+
+    public void setEspisodios(int espisodios) {
+        this.espisodios = espisodios;
+    }
+}
