f381f513b74e9e126c5b04fd3c725e79f47a45af
Rodrigo Ramos <rodrigo.oliveira@camtecnologia.com.br>
2026-01-05 15:13:40 -0300

    new java course updates

---
+++ Curso-Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/domain/Anime.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+
+        for (int episodio: this.episodios) {
+            System.out.println(episodio + " ");
+        }
+
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
