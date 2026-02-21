14961e150f6215cc51e082cd3304da03610770eb
Rodrigo <rodrigojsg9@gmail.com>
2026-02-21 16:26:40 -0300

    new updates from java course

--- Curso-Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ Curso-Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -2,9 +2,33 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] seminarios;
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getNome() {
